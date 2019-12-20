716421e17413397edef220eab2f51f4e0f3442e9
sho <sskmy1024.y@gmail.com>
2019-12-21 02:29:20 +0900

    add background geo location

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import BackgroundGeolocation, { Location } from 'react-native-background-geolocation'
 
 export default function App() {
+  useEffect(() => {
+    BackgroundGeolocation.onLocation(onLocation)
+  })
+
+  function onLocation(location: Location) {
+    console.log('[location] -', location)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
