e23489cc38010bce4a1118693bc416a9d463149d
sho <sskmy1024.y@gmail.com>
2019-12-23 23:45:57 +0900

    initial commit

--- android/app/src/main/java/com/alwayslocationgetter/MainApplication.java
+++ android/app/src/main/java/com/alwayslocationgetter/MainApplication.java
@@ -3,6 +3,7 @@ package com.alwayslocationgetter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 import com.transistorsoft.rnbackgroundgeolocation.RNBackgroundGeolocation;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBackgroundFetchPackage(),
             new RNBackgroundGeolocation(),
           new ReanimatedPackage(),
           new RNGestureHandlerPackage(),
