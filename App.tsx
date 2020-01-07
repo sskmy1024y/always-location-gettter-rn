9d7a66ac5b7d4e4d1b13c1bd5d7c88d8e833131d
sho <sskmy1024.y@gmail.com>
2020-01-07 10:48:40 +0900

    fix provider

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { SafeAreaView, AsyncStorage } from 'react-native'
-import LocationContainer from './src/containers/LocationContainer'
 import Main from './src/page/Main'
 import LoginContainer from './src/containers/LoginContainer'
 
 export default function App() {
   return (
-    <Providers>
+    <LoginContainer.Provider>
       <Main />
-    </Providers>
+    </LoginContainer.Provider>
   )
 }
-
-const Providers = ({ children }: { children?: React.ReactElement }) => (
-  <LoginContainer.Provider>
-    <LocationContainer.Provider>{children}</LocationContainer.Provider>
-  </LoginContainer.Provider>
-)
